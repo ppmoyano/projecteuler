ab638fe3106f1c578a9cd798565cd29c0de61544
Pepe Moyano <josemariamoyano.ar@gmail.com>
2019-11-30 11:54:19 -0800

    Exercise 2

--- src/codeJava/Utilidades.java
+++ src/codeJava/Utilidades.java
@@ -1,5 +1,7 @@
 package codeJava;
 
+import java.util.ArrayList;
+
 public class Utilidades {
 
     public static boolean is3Multiply(int num)
@@ -10,5 +12,45 @@ public class Utilidades {
     public static boolean is5Multiply(int num)
     {
         return (num%5) == 0 ;
+    }
+
+    public static boolean is2Multiply(int num)
+    {
+        return (num%2) == 0 ;
+    }
+
+    public static ArrayList FibonaciNumbersOrders(int count)
+    {
+        ArrayList lista = new ArrayList();
+        int suma=0;
+        int aux1=0;
+        int aux2=1;
+        int contador=0;
+        do{
+            suma = aux1 + aux2;
+            lista.add(suma);
+            aux1 = aux2;
+            aux2 = suma;
+            contador++;
+        }while(contador<count);
+
+        return lista;
+    }
+
+
+    public static ArrayList FibonaciNumbersMaxNum(int sum)
+    {
+        ArrayList lista = new ArrayList();
+        int suma=0;
+        int aux1=0;
+        int aux2=1;
+        while (suma<sum){
+            suma = aux1 + aux2;
+            lista.add(suma);
+            aux1 = aux2;
+            aux2 = suma;
+        }
+    lista.remove(lista.size()-1);
+        return lista;
     }
 }
