c6c47b8966951367cbb8252a2ce381e350222c64
Pepe Moyano <josemariamoyano.ar@gmail.com>
2019-11-30 10:55:03 -0800

    Exercise 1

---
+++ Excersise1.java
@@ -0,0 +1,19 @@
+public class Excersise1 {
+
+    public static void main(String [] args){
+        int sumatoria=0;
+        for (int i=0; i< 1000; i ++)
+        {
+            if (Utilidades.is3Multiply(i) || Utilidades.is5Multiply(i))
+            {
+                sumatoria = sumatoria + i;
+            }
+
+
+        }
+
+        System.out.println("El resultado es "+sumatoria);
+
+    }
+
+}
---
+++ Utilidades.java
@@ -0,0 +1,12 @@
+public class Utilidades {
+
+    public static boolean is3Multiply(int num)
+    {
+        return (num%3) == 0 ;
+    }
+
+    public static boolean is5Multiply(int num)
+    {
+        return (num%5) == 0 ;
+    }
+}
